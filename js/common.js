afc8cb7efd6b464d957fe4f4ba6aa93b6569ac6e
unknown <vpikta94@gmail.com>
2018-06-13 19:54:20 +0300

    Second changes

--- js/common.js
+++ js/common.js
@@ -21,5 +21,14 @@
 // Script Slider
   
 
+// Preloader
+
+$(window).on('load', function () {
+  $preloader = $('.loaderArea'),
+    $loader = $preloader.find('.loader');
+  $loader.fadeOut();
+  $preloader.delay(350).fadeOut('slow');
+});
+
 
 
