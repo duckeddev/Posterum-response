24597d818051a6ad4e7584b32613b42b4aca7126
unknown <vpikta94@gmail.com>
2018-06-13 19:28:56 +0300

    Текст вашего сообщения о том, что изменено

---
+++ js/common.js
@@ -0,0 +1,25 @@
+// Script SmothScroll
+
+  $(function () {
+    $("a[href^='#']").click(function () {
+      var _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+    });
+  });
+
+// Script Tabs
+
+  $(function () {
+    $(".tab_item").not(":first").hide();
+    $(".what-we-do-list .what-we-do-item").click(function () {
+      $(".what-we-do-list .what-we-do-item").removeClass("active").eq($(this).index()).addClass("active");
+      $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+  });
+
+// Script Slider
+  
+
+
+
